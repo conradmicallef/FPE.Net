a0eadf8de1719635c828a5eacc6f87d529cc0e87
Conrad Micallef <conradmicallef@gmail.com>
2021-11-16 07:00:10 +0100

    Migrated to .NET 4.8 Migrated to .NET 5.0

---
+++ FPE.NetCore/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0d9ec2ef-981e-428b-8b4c-e91ab99bc2cc")]
+
+
+[assembly: InternalsVisibleTo("FPE.NetCore.Test")]
